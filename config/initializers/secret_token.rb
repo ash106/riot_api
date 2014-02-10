2f81e4358b8068db1aef6a984e3e849d0ea60242
Alex Howington <alex.howington@gmail.com>
2014-02-09 19:39:06 -0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RiotApi::Application.config.secret_key_base = 'f3d7f52970fe05a4931fce854adc14af6b98675ddb3d766b66cd4159df21d79d14305b4cbc0b8f570e0bb1b79a417952dbaa93a73d1ea9709d7022b720ead691'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RiotApi::Application.config.session_store :cookie_store, key: '_riot_api_session'
